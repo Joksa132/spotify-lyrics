dc366aa76620779fb1b7e85b85cf6c8c8c0356f6
Joksa132 <nikolajoksimovic419@gmail.com>
2024-01-18 16:57:44 +0100

    create getSongInfo function and modify styles

--- scripts/content.js
+++ scripts/content.js
@@ -3,16 +3,35 @@ console.log("Content script loaded");
 let modalSelector;
 let modalExists = false;
 
+function getSongInfo() {
+  const songNameElement = document.querySelector(
+    "[data-testid='context-item-link']"
+  );
+  const songArtistsElements = document.querySelectorAll(
+    "[data-testid='context-item-info-artist']"
+  );
+  let songArtists = [];
+
+  songArtistsElements.forEach((songArtistElement) => {
+    songArtists.push(songArtistElement.textContent);
+  });
+
+  return {
+    songName: songNameElement.textContent,
+    songArtists,
+  };
+}
+
 function createModal() {
   const modal = document.createElement("div");
   modal.id = "spotify-lyrics-modal";
   modal.setAttribute(
     "style",
-    "display: flex; position: fixed; top: 0; right: 0; background: #121212; z-index: 999; padding: 10px; overflow-y: auto; height: calc(100vh - 120px); border-radius: 5px; width: 300px"
+    "display: flex; position: fixed; top: 0; right: 0; background: #1A1A1A; z-index: 999; padding: 10px; overflow-y: auto; height: calc(100vh - 120px); border-radius: 5px; width: 35%"
   );
+  modalExists = true;
   document.body.appendChild(modal);
   modalSelector = modal;
-  modalExists = true;
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
