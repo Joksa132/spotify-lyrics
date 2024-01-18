f438b1b3b7ecd59965283d18e3faf95f08b8bf1f
Joksa132 <nikolajoksimovic419@gmail.com>
2024-01-18 15:38:53 +0100

    track if modal is opened

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,9 @@
+let isOpened = false;
+
 chrome.action.onClicked.addListener(() => {
   console.log("CLICKED EXTENSION");
+  isOpened = !isOpened;
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { message: "CLICKED" });
+    chrome.tabs.sendMessage(tabs[0].id, { message: "CLICKED", isOpened });
   });
 });
